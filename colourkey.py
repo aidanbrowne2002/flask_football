5c534acbd144a591276742d09dbe9bf8d32f05ab
aidanbrowne2002 <62239752+aidanbrowne2002@users.noreply.github.com>
2023-09-25 10:59:20 +0100

    passing legend fix

--- colourkey.py
+++ colourkey.py
@@ -1,35 +1,47 @@
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
 
-def save_color_key_image(num1, num2, player1, player2, outcome, names, box_width=0.83):
-    # Create a list to hold the numbers and labels
-    fig, ax = plt.subplots()
+def save_color_key_image(value1, value2, player1, player2, outcome, names, box_width=0.83):
+    mean_value1 = np.mean(value1)
+    mean_value2 = np.mean(value2)
 
-    # Calculate the mean of the data
-    means = [np.mean(num1), np.mean(num2)]
-
-    # Customize the plot
-    ax.set_title('Threat levels', fontsize=40, color='white')
-    ax.set_ylabel('Threat', fontsize=40, color='white')
-    ax.tick_params(axis='both', labelsize=40, color='white')
-
-    for label in ax.get_xticklabels() + ax.get_yticklabels():
-        label.set_fontsize(40)
-        label.set_color('white')
-
-    ax.set_ylim(-2, 2)
-    fig.set_size_inches(12, 15)
-
-    fig.patch.set_facecolor('none')
-
-    # Plot median lines as horizontal lines
-    for i, mean in enumerate(means):
-        ax.hlines(y=mean, xmin=i + 1 - box_width / 2, xmax=i + 1 + box_width / 2,
-                  color='green', linestyle='-', linewidth=8,
-                  label=f'Mean {names[i]}: {mean:.2f}')
+    # Check if mean values are within the valid range [-2, 2]
+    if abs(mean_value1) > 2 or abs(mean_value2) > 2:
+        raise ValueError("Mean values must be between -2 and 2")
 
     if outcome == 1:
-        fig.savefig(f'static/images/color_key_success/{player1}_{player2}.png', bbox_inches='tight', transparent=True)
+        img = mpimg.imread('static/images/passing_success.png')  # Replace 'background_image.png' with your image file
     else:
-        fig.savefig(f'static/images/color_key_unsuccess/{player1}_{player2}.png', bbox_inches='tight', transparent=True)
+        img = mpimg.imread('static/images/passing_fail.png')
+
+    # Create a tall and skinny figure
+    fig, ax = plt.subplots(figsize=(3, 6))  # Adjust the width and height as needed
+
+    # Display the image
+    ax.imshow(img, extent=[-0.5, 0.5, -2, 2])  # Adjust x-axis range here
+
+    # Plot arrows with y positions based on the mean values
+    arrow_length = 0.2  # Length of the arrows
+    ax.arrow(-1, mean_value1, arrow_length, 0, head_width=0.1, head_length=0.2, fc='orange', ec='orange')  # Left to right
+    ax.arrow(1, mean_value2, -arrow_length, 0, head_width=0.1, head_length=0.2, fc='orange', ec='orange')  # Right to left
+
+    # Add mean labels near the arrows
+    ax.text(-1, mean_value1+0.15, f'{mean_value1:.2f}', ha='left', va='center', color='w', fontsize=12)
+    ax.text(1, mean_value2+0.15, f'{mean_value2:.2f}', ha='right', va='center', color='w', fontsize=12)
+
+    # Remove labels and legend
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    ax.legend().set_visible(False)
+    ax.axis('off')
+
+    if outcome == 1:
+        fig.savefig(f'static/images/color_key_success/{player1}_{player2}.png', bbox_inches='tight', transparent=True,
+                    format='png')
+    else:
+        fig.savefig(f'static/images/color_key_unsuccess/{player1}_{player2}.png', bbox_inches='tight', transparent=True,
+                    format='png')
     plt.close(fig)
