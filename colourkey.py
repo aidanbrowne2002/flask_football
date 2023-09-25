2149ab2362990d873ce6448721b092fe77e9aea3
aidanbrowne2002 <62239752+aidanbrowne2002@users.noreply.github.com>
2023-09-25 11:08:21 +0100

    fixed radar chart div 0 error

--- colourkey.py
+++ colourkey.py
@@ -7,8 +7,8 @@ def save_color_key_image(value1, value2, player1, player2, outcome, names, box_w
     mean_value2 = np.mean(value2)
 
     # Check if mean values are within the valid range [-2, 2]
-    if abs(mean_value1) > 2 or abs(mean_value2) > 2:
-        raise ValueError("Mean values must be between -2 and 2")
+    #if abs(mean_value1) > 2 or abs(mean_value2) > 2:
+        #raise ValueError("Mean values must be between -2 and 2")
 
     if outcome == 1:
         img = mpimg.imread('static/images/passing_success.png')  # Replace 'background_image.png' with your image file
